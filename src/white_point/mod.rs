2cc81f403f8ebedd6222a5f201f4317a71366318
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-01-16 12:56:23 -0500

    Refactored white points; implemented all F1-F12 illuminants.
    Data is automatically generated from the table in wikipedia,
    except for XYZs that are defined by ASTM E308 - 01.

---
+++ src/white_point/mod.rs
@@ -0,0 +1,13 @@
+// All data taken from www.brucelindbloom.com.
+
+use xyz::Xyz;
+use xyy::XyY;
+
+pub trait NamedWhitePoint<T> {
+    fn get_xyz() -> Xyz<T>;
+    fn get_xy_chromaticity() -> XyY<T>;
+}
+
+pub mod deg_2;
+
+pub use self::deg_2::*;
