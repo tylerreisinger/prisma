0cf72078e937297b1b217724ee748fa6ccf5a9d2
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-01-21 18:50:08 -0500

    Documented `ycbcr::model`

--- src/ycbcr/mod.rs
+++ src/ycbcr/mod.rs
@@ -1,3 +1,33 @@
+//! A module for encoded values in the YUV and YIQ family of spaces.
+//!
+//! YUV and YIQ refer to a collection of spaces, each derived from a parent
+//! RGB space. Fundamentally, these spaces are created from a linear transformation
+//! from a parent RGB space. There are standard definitions
+//! for both the conversion matrices as well as the RGB space to use as the parent,
+//! but many other YUV-like spaces can be constructed.
+//!
+//! Traditionally, the term `YCbCr` refers to a integral encoded digital
+//! signal within the YUV space and the term `YPbPr` refers to a normalized
+//! floating point digital signal. However, for the purposes of this library,
+//! `YCbCr` refers to both the integral and floating point representations
+//! of these colors.
+//!
+//! Both YUV and YIQ are represented by a luminosity (Y) or, more commonly,
+//! a luma (Y'), channel and two opponent chromaticity channels. Both of the
+//! chromaticity channels center at zero (for achromatic colors) and run
+//! both positive and negative.
+//!
+//! The exact canonical range for the channels
+//! varies between spaces, so we opt to normalize all chromaticity channels
+//! to a fixed [-1.0, 1.0] range for float channels. Integral channels
+//! run from 0 to `Integer::max_value()` with the central value
+//! `(Integer::max_value() >> 1) + 1` representing a neutral value.
+//!
+//! YIQ is a nearly obsolete space used for NTSC televisions. It is equivalent to
+//! a 33 degree rotation from the standard YUV plane and thus can represent the same
+//! set of colors. It is represented in this library by the type
+//! `type Yiq<T> = YCbCr<T, YiqModel>`, but provides some convenience methods to mask
+//! the fact that it shares an implementation with YCbCr.
 
 pub mod ycbcr;
 pub mod model;
--- src/ycbcr/model.rs
+++ src/ycbcr/model.rs
@@ -1,49 +1,125 @@
+//! Model definitions for YCbCr spaces.
+//!
+//! YCbCr can represent a family of spaces, and thus it requires
+//! a model to define physical colors. This module defines both
+//! the most common standard models as well as a type for the creation of
+//! custom YCbCr models.
+
 use num;
 use linalg::Matrix3;
 use channel::{PosNormalChannelScalar, NormalChannelScalar};
 use ycbcr::YCbCr;
 
+/// A coordinate shift for the components of a `YCbCr` model.
+///
+/// This is mostly used to shift integer components of a `YCbCr` color.
+/// There are some YCbCr standards that place "footroom" and "headroom"
+/// padding onto the components of a color. The shift allows for such
+/// ranges to be modeled generically.
 pub trait YCbCrShift<T> {
     fn get_shift() -> (T, T, T);
 }
 
+/// A model with matrix transformations.
 pub trait YCbCrTransform {
+    /// A transformation from Rgb to YCbCr.
     fn forward_transform(&self) -> Matrix3<f64>;
+    /// A transformation from YCbCr to Rgb.
     fn inverse_transform(&self) -> Matrix3<f64>;
 }
 
+/// An object that can transform from YCbCr to Rgb and back.
 pub trait YCbCrModel<T>: Clone + PartialEq + YCbCrTransform {
     type Shift: YCbCrShift<T>;
+    /// Return a shift to be added to each channel after conversion.
     fn shift(&self) -> (T, T, T);
 }
 
+/// A YCbCrModel that can transform a color in its space to the "canonical representation".
 pub trait Canonicalize<T>: YCbCrModel<T> {
+    /// Convert a YCbCr value into a tuple of channels in the canonical range.
+    ///
+    /// YUV and YIQ both define their chromaticity channels in a range other than
+    /// `[-1, 1]`. This function will convert from the normalized representation
+    /// to that defined by the standard being used.
     fn to_canonical_representation(from: YCbCr<T, Self>) -> (T, T, T);
 }
 
+/// A YCbCrModel that stores no data and thus can be used without an object.
 pub trait UnitModel<T>: YCbCrModel<T> {
+    /// Get the only valid object of the type.
     fn unit_value() -> Self;
 }
 
+/// A model with transformations that are defined at runtime.
+///
+/// A custom model provides greater flexibility than creating a new unit
+/// struct, but at the cost of requiring the two matrices to be stored
+/// and referenced in memory.
+///
+/// Using `CustomYCbCrModel` with `YCbCr` is generally best done with the
+/// `YCbCrCustom` type, which stores a reference to the model in addition
+/// to the channels. This adds a single pointer-sized value to each YCbCr
+/// object that uses the model.
+///
+/// If the added overhead is
+/// unacceptable, a `BareYCbCr` type exists. This type does not store any
+/// information about the model used to construct it, instead referencing the
+/// model at each point of conversion. Since the model is not known, it is
+/// possible to perform invalid conversions using different models to convert to and from
+/// Rgb, as well as to perform comparisons between colors in different models.
+/// `BareYCbCr` should therefore be used with care.
 #[derive(Clone, Debug, PartialEq)]
 pub struct CustomYCbCrModel {
     forward_transform: Matrix3<f64>,
     inverse_transform: Matrix3<f64>,
 }
 
+/// The `standard` shift, filling the full range of all channel types.
 #[derive(Clone, Debug, PartialEq)]
 pub struct StandardShift<T>(pub T);
 
+/// A model for the YIQ color space.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct YiqModel;
+/// A model for YUV using the BT.709 standard.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Bt709Model;
+/// A model for YUV used by Jpeg images.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct JpegModel;
 
+/// Build a transformation matrix for conversion
+/// from Rgb to a YCbCr space
+/// with a specified set of weight values.
+///
+/// Most YUV spaces are defined relative to 3 constants:
+///
+/// * Wr - The red channel weight.
+/// * Wg - The green channel weight.
+/// * Wb - The blue channel weight.
+///
+/// The three weights must sum to 1.0, so only two must be specified explicitly, the
+/// third can be computed.
+///
+/// These three weight values define the fractional contribution from each channel to the
+/// luminosity or luma 'Y' channel. The 'Cb' and 'Cr' contributions are then computed
+/// from these weights as well, though in a less obvious way.
+///
+/// This function takes the 'Wr' and 'Wb' weights and returns a matrix that can be used
+/// to go from Rgb to a YCbCr space defined by those constants. To go back, call the
+/// `inverse` method on the returned matrix.
+///
+/// # Panics
+///
+/// Panics if either `kr` or `kb` are negative, or if the sum of them is greater than 1.0
 pub fn build_transform<T>(kr: T, kb: T) -> Matrix3<T>
     where T: num::Float
 {
+    assert!(kr+kb < num::cast(1.0).unwrap());
+    assert!(kr >= num::cast(0.0).unwrap());
+    assert!(kb >= num::cast(0.0).unwrap());
+
     let half = num::cast::<_, T>(0.5).unwrap();
     let one = num::cast::<_, T>(1.0).unwrap();
 
@@ -62,6 +138,12 @@ pub fn build_transform<T>(kr: T, kb: T) -> Matrix3<T>
 
 
 impl CustomYCbCrModel {
+    /// Construct a model from the forward and inverse transformations.
+    ///
+    /// The forward transformation should go from Rgb to YCbCr and the
+    /// inverse should do the opposite. The matrix should be made to
+    /// output channels in the correct range, otherwise the resulting
+    /// colors will not behave as expected.
     pub fn new(forward_transform: Matrix3<f64>, inverse_transform: Matrix3<f64>) -> Self {
         CustomYCbCrModel {
             forward_transform: forward_transform,
@@ -69,6 +151,9 @@ impl CustomYCbCrModel {
         }
     }
 
+    /// Build a custom model from channel weights.
+    ///
+    /// See the `build_transform` method in the module for details on these parameters.
     pub fn build_from_coefficients(kr: f64, kb: f64) -> Self {
         let transform = build_transform(kr, kb);
         let inv_transform =
@@ -77,8 +162,7 @@ impl CustomYCbCrModel {
     }
 }
 
-impl YCbCrTransform for CustomYCbCrModel
-{
+impl YCbCrTransform for CustomYCbCrModel {
     fn forward_transform(&self) -> Matrix3<f64> {
         self.forward_transform.clone()
     }
@@ -116,8 +200,7 @@ impl<'a, T> YCbCrModel<T> for &'a CustomYCbCrModel
     }
 }
 
-impl<'a> YCbCrTransform for &'a CustomYCbCrModel
-{
+impl<'a> YCbCrTransform for &'a CustomYCbCrModel {
     fn forward_transform(&self) -> Matrix3<f64> {
         self.forward_transform.clone()
     }
@@ -135,8 +218,7 @@ impl<'a, T> Canonicalize<T> for &'a CustomYCbCrModel
     }
 }
 
-impl YCbCrTransform for Bt709Model
-{
+impl YCbCrTransform for Bt709Model {
     fn forward_transform(&self) -> Matrix3<f64> {
         Matrix3::new([0.2126,
                       0.7152,
@@ -187,8 +269,7 @@ impl<T> Canonicalize<T> for Bt709Model
     }
 }
 
-impl YCbCrTransform for YiqModel
-{
+impl YCbCrTransform for YiqModel {
     fn forward_transform(&self) -> Matrix3<f64> {
         Matrix3::new([0.299, 0.587, 0.114, 1.0, -0.4599631, -0.540541, 0.403750, -1.0, 0.597015])
     }
@@ -225,8 +306,7 @@ impl<T> Canonicalize<T> for YiqModel
     }
 }
 
-impl YCbCrTransform for JpegModel
-{
+impl YCbCrTransform for JpegModel {
     fn forward_transform(&self) -> Matrix3<f64> {
         Matrix3::new([0.299f64, 0.587, 0.114, -0.168736, -0.331264, 0.5, 0.5, -0.418688, -0.081312])
     }
